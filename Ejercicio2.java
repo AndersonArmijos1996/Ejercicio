ff119228c86624fb32509ee2b84f6374e83019ca
Anderson Armijos <52385826+AndersonArmijos1996@users.noreply.github.com>
2019-07-01 21:54:42 -0500

    Add files via upload

---
+++ Ejercicio2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Ejercicio2{
+	public static void main(String []args){
+	float num, den;
+	Scanner lee=new Scanner (System.in);
+	System.out.println("ingrese el numerador");
+	num=lee.nextFloat();
+	System.out.println("ingrese el denominador");
+	den= lee.nextFlaot ();
+	} 
+}
